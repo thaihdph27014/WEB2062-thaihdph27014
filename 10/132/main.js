8c4723d6cd62868dcac1690f0967c77f5f764232
thaihdph27014 <hoangducthai2301@gmail.com>
2023-01-30 15:42:56 +0700

    update js

---
+++ 10/132/main.js
@@ -0,0 +1,13 @@
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+greet('Hello')('Jonas');
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+greetArr('Hi')('Jonas');
